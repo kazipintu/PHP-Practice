167e0e47e339fedfce1f7790f5d9e4a8bd35c288
Kazi Afzaluddin <kazipintu@gmail.com>
2023-10-03 22:52:14 +0530

    Star Learning php

---
+++ arraying.php
@@ -0,0 +1,26 @@
+<?php
+
+$array_1 = [4,5,6,7,"kazi", 8];
+  echo $array_1[4]."<br>";// numbering index
+
+$count=0;
+for ($i = 0; $i<10; $i = $i + 1) {
+$count = $count + $i;
+  echo $count."<br>";
+}
+  echo $count."<br>"."<br>";
+// Asscociative array
+
+$asso1 = ["Serial"=>"3", "Name"=>"kazi", "Roll"=>"D232"];
+
+foreach($asso1 as $key => $val ){
+echo $key.'-'.$val."<br>";
+}
+
+echo '<br>';
+foreach($asso1 as $key){
+echo $key.'-'.$val."<br>";
+  
+}
+
+?>
---
+++ practice1.php
@@ -0,0 +1,16 @@
+<?php
+  echo "I like pizza <br>";
+  echo "It's really good <br>";
+// comment
+  $name = "Kazi Afzaluddin <br>";
+  echo $name;
+
+  $price_pizza1 = 250;
+  $price_pizza2= 340;
+
+  echo $price_pizza1. `<br>`;
+  echo $price_pizza2. `<br>`;
+  $total= $price_pizza1 + $price_pizza2;
+  echo "Total price = ".$total;
+?>
+
